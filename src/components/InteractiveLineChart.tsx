004e8eaf1e5adda903a7bdee41913d16919cd69c
TalMilnerSimilar <redinblood@gmail.com>
2025-08-09 20:22:10 +0300

    Charts: use solid horizontal grid lines (remove strokeDasharray)

--- src/components/InteractiveLineChart.tsx
+++ src/components/InteractiveLineChart.tsx
@@ -39,7 +39,7 @@ const InteractiveLineChart: React.FC<InteractiveLineChartProps> = ({
             bottom: 5,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <CartesianGrid vertical={false} />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip content={<CustomTooltip brandColorMap={brandColorMap} />} />
