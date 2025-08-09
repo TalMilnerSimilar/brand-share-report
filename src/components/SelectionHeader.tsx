b0480eddff4bb23cac96a960121848d6ef27a226
TalMilnerSimilar <redinblood@gmail.com>
2025-08-10 02:22:28 +0300

    SelectionHeader: move domain dropdown to right filters area to match placement of other dropdowns

--- src/components/SelectionHeader.tsx
+++ src/components/SelectionHeader.tsx
@@ -30,40 +30,46 @@ const SelectionHeader: React.FC = () => {
   return (
     <div className="shi-header">
       <div className="header-container">
-        {/* Left side - Title + domain dropdown */}
+        {/* Left side - Title only */}
         <div className="header-left" style={{ gap: 16, display: 'flex', alignItems: 'center' }}>
           <div className="brand-share-title">Brand Share Reports Overview</div>
-          <div className="dropdown-container" ref={domainDropdownRef} onClick={() => setIsDomainDropdownOpen(!isDomainDropdownOpen)}>
-            <div className="dropdown-header">
-              <span className="dropdown-text" style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-                <img src="/icons/us-flag.svg" alt="US" style={{ width: 16, height: 16 }} />
-                {selectedDomain}
-              </span>
-              <div className={`dropdown-icon ${isDomainDropdownOpen ? 'rotated' : ''}`}>
-                <img src="/icons/chevron-down.svg" alt="Expand" />
-              </div>
-            </div>
-            {isDomainDropdownOpen && (
-              <div className="dropdown-menu">
-                {domainOptions.map((opt) => (
-                  <div
-                    key={opt.label}
-                    className={`dropdown-item ${opt.label === selectedDomain ? 'selected' : ''}`}
-                    onClick={() => handleDomainSelect(opt.label)}
-                  >
-                    <span style={{ display: 'inline-flex', alignItems: 'center', gap: 8 }}>
-                      <img src={opt.flag} alt="flag" style={{ width: 16, height: 16 }} />
-                      {opt.label}
-                    </span>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
         </div>
 
-        {/* Right side - empty for selection screen */}
-        <div className="header-right" />
+        {/* Right side - Domain dropdown positioned like other filters */}
+        <div className="header-right">
+          <div className="filters-container">
+            <div className="filter-group">
+              <span className="filter-label">Domain</span>
+              <div className="dropdown-container" ref={domainDropdownRef}>
+                <div className="dropdown-header" onClick={() => setIsDomainDropdownOpen(!isDomainDropdownOpen)}>
+                  <span className="dropdown-text" style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+                    <img src="/icons/us-flag.svg" alt="US" style={{ width: 16, height: 16 }} />
+                    {selectedDomain}
+                  </span>
+                  <div className={`dropdown-icon ${isDomainDropdownOpen ? 'rotated' : ''}`}>
+                    <img src="/icons/chevron-down.svg" alt="Expand" />
+                  </div>
+                </div>
+                {isDomainDropdownOpen && (
+                  <div className="dropdown-menu">
+                    {domainOptions.map((opt) => (
+                      <div
+                        key={opt.label}
+                        className={`dropdown-item ${opt.label === selectedDomain ? 'selected' : ''}`}
+                        onClick={() => handleDomainSelect(opt.label)}
+                      >
+                        <span style={{ display: 'inline-flex', alignItems: 'center', gap: 8 }}>
+                          <img src={opt.flag} alt="flag" style={{ width: 16, height: 16 }} />
+                          {opt.label}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
